dfdf48271bbd842e728a3fc36c16b6fac3a87f9d
Azuzi <azuzii@proton.me>
2025-09-30 15:00:35 +0100

    feat(product): Define CreateProductInput DTO with validation.

---
+++ src/modules/product/dto/product.input.ts
@@ -0,0 +1,25 @@
+import { Field, Float, ID, InputType, PartialType } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional } from 'class-validator';
+
+@InputType()
+export class CreateProductInput {
+  @Field({ nullable: false })
+  @IsNotEmpty()
+  name!: string;
+
+  @Field({ nullable: false })
+  @IsNotEmpty()
+  code!: string;
+
+  @Field(() => Float, { nullable: false })
+  @IsNotEmpty()
+  price!: number;
+
+  @Field({ nullable: true, defaultValue: false })
+  @IsOptional()
+  isSample = false;
+
+  @Field()
+  @IsNotEmpty()
+  customer_id!: string;
+}
