12899f45f98783b96db6b4d28a0f6d01eb5f32de
Azuzi <azuzii@proton.me>
2025-09-30 21:43:13 +0200

    feat(product-service): Implement createProduct method.

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { ProductRepository } from './product.repository';
+import { DataSource, EntityManager } from 'typeorm';
+import { CreateProductDto } from './dto/product.dto';
+import { CreateProductResult } from './dto/product.type';
+import { ProductAlreadyExist } from './dto/product.error';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    private readonly repo: ProductRepository,
+    private readonly datasource: DataSource,
+  ) {}
+
+  async createProduct(
+    productDto: CreateProductDto,
+  ): Promise<typeof CreateProductResult> {
+    return this.datasource.transaction(async (entityManager: EntityManager) => {
+      try {
+        const product = await this.repo.createProduct(
+          productDto,
+          entityManager,
+        );
+
+        return product;
+      } catch (error) {
+        if (error instanceof ProductAlreadyExist) {
+          return error;
+        }
+        throw error;
+      }
+    });
+  }
+}
