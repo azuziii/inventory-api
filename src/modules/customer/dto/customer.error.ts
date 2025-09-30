aeb54a5742122efb98732de58d4c1ed95013a49f
Azuzi <azuzii@proton.me>
2025-09-30 08:56:43 +0100

    feat: create customer errors CustomerAlreadyExist, and CustomerNotFound

---
+++ src/modules/customer/dto/customer.error.ts
@@ -0,0 +1,26 @@
+import { ObjectType } from '@nestjs/graphql';
+import {
+  AlreadyExist,
+  AlreadyExistProps,
+} from 'src/common/errors/alread-exist.error';
+import { NotFound, NotFoundProps } from 'src/common/errors/not-found.error';
+
+@ObjectType()
+export class CustomerAlreadyExist extends AlreadyExist {
+  constructor(props: AlreadyExistProps) {
+    super({
+      entityType: 'Customer',
+      ...props,
+    });
+  }
+}
+
+@ObjectType()
+export class CustomerNotFound extends NotFound {
+  constructor(props: NotFoundProps) {
+    super({
+      entityType: 'Customer',
+      ...props,
+    });
+  }
+}
