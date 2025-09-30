c688edba40630c8d030541eedaa658a019aa84bc
Azuzi <azuzii@proton.me>
2025-09-30 08:53:32 +0100

    chore: configure GraphQLModule, ConfigModule, and TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomerModule } from './modules/customer/customer.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      debug: true,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('DB_HOST'),
+        port: +configService.getOrThrow('DB_PORT'),
+        username: configService.getOrThrow('DB_USERNAME'),
+        password: configService.getOrThrow('DB_PASSWORD'),
+        database: configService.getOrThrow('DB_NAME'),
+        entities: [__dirname + '/**/*.entity.js'],
+        synchronize: true,
+        logging: 'all',
+        logger: 'debug',
+      }),
+    }),
+    CustomerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
