2ff1bbb3f0f68d4b76b7bf747d312eca88c3924c
Azuzi <azuzii@proton.me>
2025-09-30 21:45:03 +0200

    feat(customer-error): implement ProductNotFound

--- src/modules/product/dto/product.error.ts
+++ src/modules/product/dto/product.error.ts
@@ -2,9 +2,19 @@ import {
   AlreadyExist,
   AlreadyExistProps,
 } from 'src/common/errors/alread-exist.error';
+import { NotFound, NotFoundProps } from 'src/common/errors/not-found.error';
 
 export class ProductAlreadyExist extends AlreadyExist {
   constructor(props: AlreadyExistProps) {
+    super({
+      entityType: 'Product',
+      ...props,
+    });
+  }
+}
+
+export class ProductNotFound extends NotFound {
+  constructor(props: NotFoundProps) {
     super({
       entityType: 'Product',
       ...props,
