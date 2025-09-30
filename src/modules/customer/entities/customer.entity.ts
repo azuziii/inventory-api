70d63e5dd701166e9e8355a371f96ad648f29bcf
Azuzi <azuzii@proton.me>
2025-09-30 08:57:27 +0100

    feat: create customer entity

---
+++ src/modules/customer/entities/customer.entity.ts
@@ -0,0 +1,40 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { BaseUUIDEntity } from 'src/common/base/base.entity';
+import { Column, Entity, Index } from 'typeorm';
+
+@ObjectType()
+@Entity('Customer')
+export class Customer extends BaseUUIDEntity {
+  @Field({ nullable: false })
+  @Column({ type: 'text' })
+  name!: string;
+
+  @Field({ nullable: false })
+  @Column({ type: 'text' })
+  address!: string;
+
+  @Field({ nullable: false })
+  @Column({ type: 'text' })
+  city!: string;
+
+  @Field({ nullable: false })
+  @Column({ type: 'text' })
+  country!: string;
+
+  @Field({ nullable: false })
+  @Index('UQ_customer_ice', { unique: true })
+  @Column({ type: 'text' })
+  ice!: string;
+
+  @Field({ defaultValue: '' })
+  @Column({ type: 'text', default: '' })
+  contact_name!: string;
+
+  @Field({ defaultValue: '' })
+  @Column({ type: 'text', default: '' })
+  contact_phone!: string;
+
+  @Field({ defaultValue: '' })
+  @Column({ type: 'text', default: '' })
+  contact_email!: string;
+}
