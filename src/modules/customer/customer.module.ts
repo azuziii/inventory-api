4862af737dcb5ede2fba5b17908ab5a37a52d83d
Azuzi <azuzii@proton.me>
2025-09-30 22:15:34 +0200

    feat(customer-module): register ICustomer as a provider

--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -4,9 +4,18 @@ import { CustomerResolver } from './customer.resolver';
 import { CustomerRepository } from './customer.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Customer } from './entities/customer.entity';
+import { ICustomer } from './interfaces/customer.interface';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Customer])],
-  providers: [CustomerResolver, CustomerService, CustomerRepository],
+  providers: [
+    CustomerResolver,
+    CustomerService,
+    CustomerRepository,
+    {
+      provide: ICustomer,
+      useExisting: CustomerService,
+    },
+  ],
 })
 export class CustomerModule {}
