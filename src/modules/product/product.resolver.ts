5c3dfda0477eede890dc2b02cf02af17c6be3d48
Azuzi <azuzii@proton.me>
2025-10-01 09:59:48 +0100

    feat(product-resolver): Define `getProduct` method, and a field resolver for `customer`

--- src/modules/product/product.resolver.ts
+++ src/modules/product/product.resolver.ts
@@ -1,8 +1,33 @@
-import { Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  ID,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { ProductService } from './product.service';
 import { Product } from './entities/product.entity';
+import { ProductQueryResponse } from './dto/product.type';
+import { Customer } from '../customer/entities/customer.entity';
 
 @Resolver(() => Product)
 export class ProductResolver {
   constructor(private readonly productService: ProductService) {}
+
+  @Query(() => ProductQueryResponse, { name: 'productResponse' })
+  async getProduct(
+    @Args('id', { type: () => ID }) id: string,
+  ): Promise<ProductQueryResponse> {
+    const product = await this.productService.getProductOrFail(id);
+
+    return {
+      product,
+    };
+  }
+
+  @ResolveField(() => Customer)
+  customer(@Parent() product: Product): Promise<Customer> {
+    return product.customer as unknown as Promise<Customer>;
+  }
 }
