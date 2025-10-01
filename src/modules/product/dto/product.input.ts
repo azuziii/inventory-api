ed4bb71c6efbf6b5ee16f7b17bc5b0076f85b8fc
Azuzi <azuzii@proton.me>
2025-10-01 12:26:48 +0100

    feat(product-input): Define ProductArguments for filtering and pagination

--- src/modules/product/dto/product.input.ts
+++ src/modules/product/dto/product.input.ts
@@ -1,5 +1,15 @@
-import { Field, Float, ID, InputType, PartialType } from '@nestjs/graphql';
+import {
+  ArgsType,
+  Field,
+  Float,
+  ID,
+  InputType,
+  PartialType,
+} from '@nestjs/graphql';
 import { IsNotEmpty, IsOptional } from 'class-validator';
+import { FindManyOptions, ILike } from 'typeorm';
+import { Product } from '../entities/product.entity';
+import { PaginationInput } from 'src/common/dto/pagination.dto';
 
 @InputType()
 export class CreateProductInput {
@@ -29,4 +39,41 @@ export class UpdateProductInput extends PartialType(CreateProductInput) {
   @Field(() => ID, { nullable: false })
   @IsNotEmpty()
   id!: string;
+}
+
+@ArgsType()
+export class ProductArguments extends PaginationInput {
+  @Field(() => ID, { nullable: true })
+  @IsOptional()
+  id?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  name?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  code?: string;
+
+  @Field(() => Float, { nullable: true })
+  @IsOptional()
+  price?: number;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  isSample?: boolean;
+
+  toManyOptions(): FindManyOptions<Product> {
+    return {
+      where: {
+        ...(this.id ? { id: this.id } : null),
+        ...(this.name ? { name: ILike(`%${this.name}%`) } : null),
+        ...(this.code ? { code: ILike(`%${this.code}%`) } : null),
+        ...(this.price != null ? { price: this.price } : null),
+        ...(this.isSample != null ? { isSample: this.isSample } : null),
+      },
+      skip: (this.page - 1) * this.limit,
+      take: this.limit,
+    };
+  }
 }
