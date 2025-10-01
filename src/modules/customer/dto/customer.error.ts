f2143e172acc95b33d30852c8e48785f75f4a775
Azuzi <azuzii@proton.me>
2025-10-01 10:32:55 +0100

    feat(customer-error): define CustomerInUse error

--- src/modules/customer/dto/customer.error.ts
+++ src/modules/customer/dto/customer.error.ts
@@ -3,6 +3,7 @@ import {
   AlreadyExist,
   AlreadyExistProps,
 } from 'src/common/errors/alread-exist.error';
+import { InUse, InUseProps } from 'src/common/errors/in-use.error';
 import { NotFound, NotFoundProps } from 'src/common/errors/not-found.error';
 
 @ObjectType()
@@ -18,6 +19,16 @@ export class CustomerAlreadyExist extends AlreadyExist {
 @ObjectType()
 export class CustomerNotFound extends NotFound {
   constructor(props: NotFoundProps) {
+    super({
+      entityType: 'Customer',
+      ...props,
+    });
+  }
+}
+
+@ObjectType()
+export class CustomerInUse extends InUse {
+  constructor(props: InUseProps) {
     super({
       entityType: 'Customer',
       ...props,
