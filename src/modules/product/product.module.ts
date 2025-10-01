a4bbc8ca1794637614dc2b2f3d4a0ed148808bc4
Azuzi <azuzii@proton.me>
2025-10-01 09:54:23 +0100

    feat(product-module): Import CustomerModule

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -4,9 +4,10 @@ import { ProductResolver } from './product.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { ProductRepository } from './product.repository';
+import { CustomerModule } from '../customer/customer.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product]), CustomerModule],
   providers: [ProductResolver, ProductService, ProductRepository],
 })
 export class ProductModule {}
