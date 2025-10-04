b4ed0f189857de8ab466c777f3eadb4f410e73f0
Azuzi <azuzii@proton.me>
2025-10-04 11:22:17 +0100

    feat(common-error): Create `InvalidData` error class

---
+++ src/common/errors/invalid-data.error.ts
@@ -0,0 +1,20 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { BaseError } from './error';
+
+export interface InvalidDataProps {
+  message?: string;
+}
+
+@ObjectType()
+export class InvalidData extends BaseError implements InvalidDataProps {
+  static readonly __typename: string = 'InvalidData';
+
+  constructor(props: InvalidDataProps) {
+    super({
+      code: 'INVALID_DATA',
+      message: `Invlaid data`,
+    });
+
+    Object.assign(this, props);
+  }
+}
