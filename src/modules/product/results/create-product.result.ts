27d344a135fc3d2ab62b243772d466a38dfa069f
Azuzi <azuzii@proton.me>
2025-10-04 11:46:39 +0100

    fix(product-union): Add InvalidData to `CreateProductResult` and `UpdateProductResult` and  union

--- src/modules/product/results/create-product.result.ts
+++ src/modules/product/results/create-product.result.ts
@@ -2,16 +2,21 @@ import { AlreadyExist } from 'src/common/errors/already-exist.error';
 import { Product } from '../entities/product.entity';
 import { CustomerNotFound } from 'src/modules/customer/errors/customer.error';
 import { createUnionType } from '@nestjs/graphql';
+import { InvalidData } from 'src/common/errors/invalid-data.error';
 
 export const CreateProductResult = createUnionType({
   name: 'CreateProductResult',
-  types: () => [Product, AlreadyExist, CustomerNotFound],
-  resolveType: (value: Product | AlreadyExist | CustomerNotFound) => {
+  types: () => [Product, AlreadyExist, CustomerNotFound, InvalidData],
+  resolveType: (
+    value: Product | AlreadyExist | CustomerNotFound | InvalidData,
+  ) => {
     switch (value.constructor['__typename']) {
       case 'AlreadyExist':
         return AlreadyExist;
       case 'CustomerNotFound':
         return CustomerNotFound;
+      case 'InvalidData':
+        return InvalidData;
     }
 
     if ('price' in value) {
