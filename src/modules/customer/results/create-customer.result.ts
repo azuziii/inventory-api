39a793eda7efebac3f1a8a96c3bab7e6c6eb4d5a
Azuzi <azuzii@proton.me>
2025-10-05 16:04:19 +0200

    feat(customer-result): Add `InvalidData` to customer create and update results

--- src/modules/customer/results/create-customer.result.ts
+++ src/modules/customer/results/create-customer.result.ts
@@ -1,8 +1,9 @@
 import { AlreadyExist } from 'src/common/errors/already-exist.error';
 import { Customer } from '../entities/customer.entity';
 import { createUnionType } from '@nestjs/graphql';
+import { InvalidData } from 'src/common/errors/invalid-data.error';
 
 export const CreateCustomerResult = createUnionType({
   name: 'CreateCustomerResult',
-  types: () => [Customer, AlreadyExist],
+  types: () => [Customer, AlreadyExist, InvalidData],
 });
--- src/modules/customer/results/update-customer.result.ts
+++ src/modules/customer/results/update-customer.result.ts
@@ -2,8 +2,9 @@ import { NotFound } from 'src/common/errors/not-found.error';
 import { Customer } from '../entities/customer.entity';
 import { AlreadyExist } from 'src/common/errors/already-exist.error';
 import { createUnionType } from '@nestjs/graphql';
+import { InvalidData } from 'src/common/errors/invalid-data.error';
 
 export const UpdateCustomerResult = createUnionType({
   name: 'UpdateCustomerResult',
-  types: () => [Customer, NotFound, AlreadyExist],
+  types: () => [Customer, NotFound, AlreadyExist, InvalidData],
 });
