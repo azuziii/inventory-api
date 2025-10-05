e8a867e8de20091578f5c5baf6d0faca05ce8be5
Azuzi <azuzii@proton.me>
2025-10-05 21:45:35 +0200

    refactor(product-dto): Remove `CustomerNotFound` from `CreateProductDto` interface
    Removes the unnecessary `CustomerNotFound` type from the 'customer' property in the `CreateProductDto` interface.

--- src/modules/product/dto/product.dto.ts
+++ src/modules/product/dto/product.dto.ts
@@ -1,12 +1,11 @@
 import { Customer } from 'src/modules/customer/entities/customer.entity';
-import { CustomerNotFound } from 'src/modules/customer/errors/customer.error';
 
 export interface CreateProductDto {
   name: string;
   code: string;
   price: number;
   isSample: boolean;
-  customer: Customer | CustomerNotFound;
+  customer: Customer;
 }
 
 export interface UpdateProductDto extends Partial<CreateProductDto> {
