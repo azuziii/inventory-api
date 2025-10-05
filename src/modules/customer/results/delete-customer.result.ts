de2275a2d0d49d24cdc14eaef80ee0acd21f8e6e
Azuzi <azuzii@proton.me>
2025-10-05 19:27:05 +0200

    fix(resulsts): Add `InvalidData` to all union results

--- src/modules/customer/results/delete-customer.result.ts
+++ src/modules/customer/results/delete-customer.result.ts
@@ -1,8 +1,9 @@
 import { createUnionType } from '@nestjs/graphql';
 import { DeleteResponse } from 'src/common/responses/delete.response';
 import { InUse } from 'src/common/errors/in-use.error';
+import { InvalidData } from 'src/common/errors/invalid-data.error';
 
 export const DeleteCustomerResult = createUnionType({
   name: 'DeleteCustomerResult',
-  types: () => [DeleteResponse, InUse],
+  types: () => [DeleteResponse, InUse, InvalidData],
 });
--- src/modules/customer/results/query-customer.result.ts
+++ src/modules/customer/results/query-customer.result.ts
@@ -1,8 +1,9 @@
 import { createUnionType } from '@nestjs/graphql';
 import { Customer } from '../entities/customer.entity';
 import { NotFound } from 'src/common/errors/not-found.error';
+import { InvalidData } from 'src/common/errors/invalid-data.error';
 
 export const CustomerQueryResult = createUnionType({
   name: 'CustomerQueryResult',
-  types: () => [Customer, NotFound],
+  types: () => [Customer, NotFound, InvalidData],
 });
