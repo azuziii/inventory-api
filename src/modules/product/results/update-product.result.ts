1b8093af32cc2083a68d7899d1a7781bcf884487
Azuzi <azuzii@proton.me>
2025-10-05 21:37:17 +0200

    refactor(product-union): Replace generic base errors with entity-specific types
    Updates all relevant Customer GraphQL Unions to replace generic base error types (AlreadyExist, NotFound) with their specific, factory-generated counterparts (ProductAlreadyExist, ProductNotFound).

--- src/modules/product/results/update-product.result.ts
+++ src/modules/product/results/update-product.result.ts
@@ -1,31 +1,16 @@
 import { createUnionType } from '@nestjs/graphql';
-import { Product } from '../entities/product.entity';
-import { NotFound } from 'src/common/errors/not-found.error';
-import { AlreadyExist } from 'src/common/errors/already-exist.error';
-import { CustomerNotFound } from 'src/modules/customer/errors/customer.error';
 import { InvalidData } from 'src/common/errors/invalid-data.error';
+import { CustomerNotFound } from 'src/modules/customer/errors/customer.error';
+import { Product } from '../entities/product.entity';
+import { ProductAlreadyExist, ProductNotFound } from '../errors/product.error';
 
 export const UpdateProductResult = createUnionType({
   name: 'UpdateProductResult',
-  types: () => [Product, NotFound, AlreadyExist, CustomerNotFound, InvalidData],
-  resolveType: (
-    value: Product | NotFound | AlreadyExist | CustomerNotFound | InvalidData,
-  ) => {
-    switch (value.constructor['__typename']) {
-      case 'NotFound':
-        return NotFound;
-      case 'AlreadyExist':
-        return AlreadyExist;
-      case 'CustomerNotFound':
-        return CustomerNotFound;
-      case 'InvalidData':
-        return InvalidData;
-    }
-
-    if ('price' in value) {
-      return Product;
-    }
-
-    return null;
-  },
+  types: () => [
+    Product,
+    ProductNotFound,
+    ProductAlreadyExist,
+    CustomerNotFound,
+    InvalidData,
+  ],
 });
