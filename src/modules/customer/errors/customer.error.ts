c8d81638a9e2a697d833001657347596207caa6d
Azuzi <azuzii@proton.me>
2025-10-06 18:12:33 +0100

    refactor(errors): rename `ENTITY_TYPE` to `ENTITY_NAME`

--- src/modules/customer/errors/customer.error.ts
+++ src/modules/customer/errors/customer.error.ts
@@ -2,8 +2,8 @@ import { AlreadyExist } from 'src/common/errors/already-exist.error';
 import { InUse } from 'src/common/errors/in-use.error';
 import { NotFound } from 'src/common/errors/not-found.error';
 
-const ENTITY_TYPE = 'Customer';
+const ENTITY_NAME = 'Customer';
 
-export const CustomerAlreadyExist = AlreadyExist(ENTITY_TYPE);
-export const CustomerNotFound = NotFound(ENTITY_TYPE);
-export const CustomerInUse = InUse(ENTITY_TYPE);
+export const CustomerAlreadyExist = AlreadyExist(ENTITY_NAME);
+export const CustomerNotFound = NotFound(ENTITY_NAME);
+export const CustomerInUse = InUse(ENTITY_NAME);
--- src/modules/product/errors/product.error.ts
+++ src/modules/product/errors/product.error.ts
@@ -2,8 +2,8 @@ import { AlreadyExist } from 'src/common/errors/already-exist.error';
 import { InUse } from 'src/common/errors/in-use.error';
 import { NotFound } from 'src/common/errors/not-found.error';
 
-const ENTITY_TYPE = 'Product';
+const ENTITY_NAME = 'Product';
 
-export const ProductAlreadyExist = AlreadyExist(ENTITY_TYPE);
-export const ProductNotFound = NotFound(ENTITY_TYPE);
-export const ProductInUse = InUse(ENTITY_TYPE);
+export const ProductAlreadyExist = AlreadyExist(ENTITY_NAME);
+export const ProductNotFound = NotFound(ENTITY_NAME);
+export const ProductInUse = InUse(ENTITY_NAME);
