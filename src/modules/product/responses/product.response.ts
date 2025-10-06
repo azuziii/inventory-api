262493706e6fe343681f41cf3bc466f9c6071a50
Azuzi <azuzii@proton.me>
2025-10-06 09:14:32 +0100

    feat(product-response): Implement `DeleteProductResponse` object type

--- src/modules/product/responses/product.response.ts
+++ src/modules/product/responses/product.response.ts
@@ -4,6 +4,7 @@ import { Product } from '../entities/product.entity';
 import { CreateProductResult } from '../results/create-product.result';
 import { ProductQueryResult } from '../results/query-product.result';
 import { UpdateProductResult } from '../results/update-product.result';
+import { DeleteProductResult } from '../results/delete-product.result';
 
 @ObjectType()
 export class ProductQueryResponse {
@@ -47,4 +48,14 @@ export class UpdateProductResponse {
 
   @Field(() => UpdateProductResult)
   product!: typeof UpdateProductResult;
+}
+
+@ObjectType()
+export class DeleteProductResponse {
+  constructor(deleteResult: typeof DeleteProductResult) {
+    this.product = deleteResult;
+  }
+
+  @Field(() => DeleteProductResult)
+  product!: typeof DeleteProductResult;
 }
