f1d63960d59076514d9b446839b7055a2a0d8b5a
Azuzi <azuzii@proton.me>
2025-10-06 09:13:20 +0100

    feat(product-errors): Create `ProductInUse` error

--- src/modules/product/errors/product.error.ts
+++ src/modules/product/errors/product.error.ts
@@ -1,7 +1,9 @@
 import { AlreadyExist } from 'src/common/errors/already-exist.error';
+import { InUse } from 'src/common/errors/in-use.error';
 import { NotFound } from 'src/common/errors/not-found.error';
 
 const ENTITY_TYPE = 'Product';
 
 export const ProductAlreadyExist = AlreadyExist(ENTITY_TYPE);
 export const ProductNotFound = NotFound(ENTITY_TYPE);
+export const ProductInUse = InUse(ENTITY_TYPE);
