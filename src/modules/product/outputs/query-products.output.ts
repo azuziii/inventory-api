a7eebe3a003063241e3c153b8d4274e72ce75880
Azuzi <azuzii@proton.me>
2025-10-08 12:30:16 +0100

    refactor(product-output): Rename 'products' field to 'items'

--- src/modules/product/outputs/query-products.output.ts
+++ src/modules/product/outputs/query-products.output.ts
@@ -5,12 +5,12 @@ import { Product } from '../entities/product.entity';
 @ObjectType()
 export class ProductList {
   constructor(products: Product[], paginationProps: PaginationProps) {
-    this.products = products;
+    this.items = products;
     this.pagination = new PaginationDto(paginationProps);
   }
 
   @Field(() => [Product!]!)
-  products!: Product[];
+  items!: Product[];
 
   @Field(() => PaginationDto)
   pagination!: PaginationDto;
