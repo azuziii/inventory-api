78d723ab2379ef5549556593f0eb486f15c91caf
Azuzi <azuzii@proton.me>
2025-10-09 21:32:46 +0100

    feat(repository): Centralize and abstract database error handling

--- src/shared/base/repository.ts
+++ src/shared/base/repository.ts
@@ -1,4 +1,11 @@
-import { EntityManager, ObjectLiteral, Repository } from 'typeorm';
+import { InternalServerErrorException } from '@nestjs/common';
+import { DatabaseError } from 'pg';
+import {
+  EntityManager,
+  ObjectLiteral,
+  QueryFailedError,
+  Repository,
+} from 'typeorm';
 
 export abstract class BaseRepositoty<
   Entity extends ObjectLiteral,
@@ -13,4 +20,20 @@ export abstract class BaseRepositoty<
   getManager(externalManagr?: EntityManager): EntityManager {
     return externalManagr || this.entityManager;
   }
+
+  handleDatabaseError(error: any, entity?: Partial<Entity>) {
+    if (!(error instanceof QueryFailedError)) throw error;
+
+    if (!(error.driverError instanceof DatabaseError)) {
+      console.error('Invalid database driver');
+      throw new InternalServerErrorException();
+    }
+
+    this.translateDatabaseError(error, entity);
+  }
+
+  protected abstract translateDatabaseError(
+    error: any,
+    entity?: Partial<Entity>,
+  ): void;
 }
