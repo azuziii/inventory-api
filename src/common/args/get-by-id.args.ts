ada5f5062287264615b1fb53b6f6682dd3a43126
Azuzi <azuzii@proton.me>
2025-10-09 09:21:09 +0100

    feat(args): Enhance UUID validation with internationalized error message

--- src/common/args/get-by-id.args.ts
+++ src/common/args/get-by-id.args.ts
@@ -1,10 +1,13 @@
 import { ArgsType, Field, ID } from '@nestjs/graphql';
 import { IsNotEmpty, IsUUID } from 'class-validator';
+import { i18nValidationMessage } from 'nestjs-i18n';
 
 @ArgsType()
 export class GetByIdArgs {
   @Field(() => ID, { nullable: false })
   @IsNotEmpty()
-  @IsUUID()
+  @IsUUID('all', {
+    message: i18nValidationMessage('validation.invalid_uuid'),
+  })
   id!: string;
 }
