e27cc092dcc796ee28f524dcfa62e75c947ca370
Azuzi <azuzii@proton.me>
2025-10-09 12:27:51 +0100

    feat(product): Register ProductSubscriber

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerModule } from '../customer/customer.module';
 import { Product } from './entities/product.entity';
+import { ProductSubscriber } from './entities/product.subscriber';
 import { ProductRepository } from './product.repository';
 import { ProductResolver } from './product.resolver';
 import { ProductService } from './product.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product]), CustomerModule],
-  providers: [ProductResolver, ProductService, ProductRepository],
+  providers: [
+    ProductResolver,
+    ProductService,
+    ProductRepository,
+    ProductSubscriber,
+  ],
 })
 export class ProductModule {}
