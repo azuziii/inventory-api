4411cc99448505d41e3ecdc4f8a0477954af2dd0
Azuzi <azuzii@proton.me>
2025-10-10 22:24:47 +0100

    feat(product-input): enable translatable validation message for customer_id UUID

--- src/modules/product/inputs/product.input.ts
+++ src/modules/product/inputs/product.input.ts
@@ -1,5 +1,6 @@
 import { Field, Float, ID, InputType, PartialType } from '@nestjs/graphql';
 import { IsNotEmpty, IsOptional, IsUUID } from 'class-validator';
+import { i18nValidationMessage } from 'nestjs-i18n';
 import { UpdateCustomerDto } from 'src/modules/customer/dto/customer.dto';
 import { Customer } from 'src/modules/customer/entities/customer.entity';
 import { WithUuidInputMixin } from 'src/shared/inputs/get-by-id/get-by-id.mixin';
@@ -25,7 +26,9 @@ export class CreateProductInput implements CreateProductDto {
 
   @Field()
   @IsNotEmpty()
-  @IsUUID()
+  @IsUUID('all', {
+    message: i18nValidationMessage('validation.invalid_uuid'),
+  })
   customer_id!: string;
 
   customer!: Customer;
