4ea9279597043a6dd59d16bc5550503ff66c0771
Azuzi <azuzii@proton.me>
2025-10-12 12:22:05 +0100

    feat(order-dto): Define `CreateOrderDto` for creating order

---
+++ src/modules/order/dto/order.dto.ts
@@ -0,0 +1,7 @@
+import { Customer } from 'src/modules/customer/entities/customer.entity';
+
+export interface CreateOrderDto {
+  order_number: number;
+  order_date: Date;
+  customer: Customer;
+}
