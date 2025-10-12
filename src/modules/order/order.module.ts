6b7ed41c399d7df39ed8599b9b058ae1358f59c3
Azuzi <azuzii@proton.me>
2025-10-12 18:22:44 +0100

    feat(order): Register `OrderResolver`, `OrderService`, `OrderRepositorym` and import `CustomerModule`

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerModule } from '../customer/customer.module';
 import { Order } from './entities/order.entity';
+import { OrderRepository } from './order.repository';
+import { OrderResolver } from './order.resolver';
+import { OrderService } from './order.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([Order]), CustomerModule],
+  providers: [OrderResolver, OrderService, OrderRepository],
 })
 export class OrderModule {}
