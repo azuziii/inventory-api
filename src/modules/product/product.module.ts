fed292779a3d6ee375eceed7bf485100d87db6f1
Azuzi <azuzii@proton.me>
2025-10-14 14:20:23 +0100

    feat(product): Register and export `IProduct`

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerModule } from '../customer/customer.module';
 import { Product } from './entities/product.entity';
 import { ProductSubscriber } from './entities/product.subscriber';
+import { IProduct } from './interfaces/product.interface';
 import { ProductRepository } from './product.repository';
 import { ProductResolver } from './product.resolver';
 import { ProductService } from './product.service';
@@ -14,6 +15,11 @@ import { ProductService } from './product.service';
     ProductService,
     ProductRepository,
     ProductSubscriber,
+    {
+      provide: IProduct,
+      useExisting: ProductService,
+    },
   ],
+  exports: [IProduct],
 })
 export class ProductModule {}
