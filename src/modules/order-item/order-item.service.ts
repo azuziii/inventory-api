86eb696c1d721edb23e39d605a52152e790b9ba7
Azuzi <azuzii@proton.me>
2025-10-14 14:49:11 +0100

    feat(order-item-service): Add `getOrderItemOrFail` to  `OrderItemService`

--- src/modules/order-item/order-item.service.ts
+++ src/modules/order-item/order-item.service.ts
@@ -5,6 +5,7 @@ import { OrderItem } from './entities/order-item.entity';
 import { OrderItemNotFound } from './errors/order-item.error';
 import { OrderItemRepository } from './order-item.repository';
 import { CreateOrderItemUnion } from './unions/create-order-item.union';
+import { OrderItemQueryUnion } from './unions/query-order-item.union';
 import { UpdateOrderItemUnion } from './unions/update-order-item.union';
 
 @Injectable()
@@ -47,5 +48,21 @@ export class OrderItemService {
       );
       return updatedOrderItem;
     });
+  }
+
+  async getOrderItemOrFail(id: string): Promise<OrderItemQueryUnion> {
+    const orderItem = await this.repo.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!orderItem) {
+      throw new OrderItemNotFound({
+        id,
+      });
+    }
+
+    return orderItem;
   }
 }
