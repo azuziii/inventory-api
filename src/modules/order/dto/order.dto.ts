d217960a1bcae2e90cf821b6b311124ef49ae508
Azuzi <azuzii@proton.me>
2025-10-14 16:43:32 +0100

    feat(order-dto): Add `items` property to `CreateOrderDto`

--- src/modules/order/dto/order.dto.ts
+++ src/modules/order/dto/order.dto.ts
@@ -1,9 +1,11 @@
 import { Customer } from 'src/modules/customer/entities/customer.entity';
+import { CreateOrderItemDto } from 'src/modules/order-item/dto/order-item.dto';
 
 export interface CreateOrderDto {
   order_number: number;
   order_date: Date;
   customer: Customer;
+  items: CreateOrderItemDto[];
 }
 
 export interface UpdateOrderDto extends Partial<CreateOrderDto> {
