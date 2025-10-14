12a84a4b000878462a1405b1f22d35c94692aeb0
Azuzi <azuzii@proton.me>
2025-10-14 14:22:16 +0100

    feat(order-item-service): Create `OrderItemService` with `createOrderItem`, and `updateOrderItem`

---
+++ src/modules/order-item/order-item.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, EntityManager } from 'typeorm';
+import { CreateOrderItemDto, UpdateOrderItemDto } from './dto/order-item.dto';
+import { OrderItem } from './entities/order-item.entity';
+import { OrderItemNotFound } from './errors/order-item.error';
+import { OrderItemRepository } from './order-item.repository';
+import { CreateOrderItemUnion } from './unions/create-order-item.union';
+import { UpdateOrderItemUnion } from './unions/update-order-item.union';
+
+@Injectable()
+export class OrderItemService {
+  constructor(
+    private readonly repo: OrderItemRepository,
+    private readonly datasource: DataSource,
+  ) {}
+
+  async createOrderItem(
+    orderItemDto: CreateOrderItemDto,
+  ): Promise<CreateOrderItemUnion> {
+    return this.datasource.transaction(async (entityManager: EntityManager) => {
+      const orderItem = await this.repo.createOrderItem(
+        orderItemDto,
+        entityManager,
+      );
+
+      return orderItem;
+    });
+  }
+
+  async updateOrderItem(
+    orderItemDto: UpdateOrderItemDto,
+  ): Promise<UpdateOrderItemUnion> {
+    return this.datasource.transaction(async (entityManager: EntityManager) => {
+      const orderItem = await entityManager.findOne(OrderItem, {
+        where: { id: orderItemDto.id },
+      });
+
+      if (!orderItem) {
+        throw new OrderItemNotFound({
+          id: orderItemDto.id,
+        });
+      }
+
+      const updatedOrderItem = await this.repo.updateOrderItem(
+        orderItemDto,
+        entityManager,
+      );
+      return updatedOrderItem;
+    });
+  }
+}
