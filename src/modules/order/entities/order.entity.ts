bc8d16dbfc1aec44263d8661fbcd7b1d9f738b33
Azuzi <azuzii@proton.me>
2025-10-22 14:06:21 +0100

    feat(order-entity): Create order entity

---
+++ src/modules/order/entities/order.entity.ts
@@ -0,0 +1,30 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Customer } from 'src/modules/customer/entities/customer.entity';
+import { BaseUUIDEntity } from 'src/shared/base/base.entity';
+import { BeforeInsert, Column, Entity, Index, ManyToOne } from 'typeorm';
+
+@Entity('order')
+@Index('UQ_ORDER_NUMBER_YEAR', ['order_year', 'order_number'], { unique: true })
+@ObjectType()
+export class Order extends BaseUUIDEntity {
+  @Field(() => Int, { nullable: false })
+  @Column({ type: 'text' })
+  order_number!: number;
+
+  @Field(() => Date, { nullable: false })
+  @Column({ type: 'timestamp' })
+  order_date!: Date;
+
+  @Field({ nullable: false })
+  @Column({ type: 'text' })
+  order_year!: string;
+
+  @Field(() => Customer, { nullable: false })
+  @ManyToOne(() => Customer, { lazy: true })
+  customer!: Customer;
+
+  @BeforeInsert()
+  setOrderYear() {
+    this.order_year = new Date(this.order_date).getFullYear().toString();
+  }
+}
