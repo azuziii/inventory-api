9c892e34d29e5847607ade1dcfee0c1ab97c7b61
Azuzi <azuzii@proton.me>
2025-10-23 14:58:56 +0100

    feat(order-item-entity): Create `OrderItem` entity class

---
+++ src/modules/order-item/entities/order-item.entity.ts
@@ -0,0 +1,31 @@
+import { Field, Float, Int, ObjectType } from '@nestjs/graphql';
+import { Product } from 'src/modules/product/entities/product.entity';
+import { BaseUUIDEntity } from 'src/shared/base/base.entity';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+
+@Entity('order_item')
+@ObjectType()
+export class OrderItem extends BaseUUIDEntity {
+  @Field()
+  @Column({ type: 'text', nullable: false })
+  product_name!: string;
+
+  @Field(() => Float)
+  @Column({ type: 'float', nullable: false })
+  product_price!: number;
+
+  @Field(() => Int, { defaultValue: 0 })
+  @Column({ type: 'int', nullable: false, default: 0 })
+  quantity!: number;
+
+  @Field(() => Product)
+  @ManyToOne(() => Product, { lazy: true })
+  @JoinColumn({
+    name: 'product_id',
+    foreignKeyConstraintName: 'FK_PRODUCT',
+  })
+  product!: Product;
+
+  @Column({ type: 'uuid' })
+  product_id!: string;
+}
