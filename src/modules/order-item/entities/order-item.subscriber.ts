778079e0d13810518559f219e6d7462105d922b1
Azuzi <azuzii@proton.me>
2025-10-23 15:07:36 +0100

    feat(order-entity-subscriber): Create `OrderItemSubscriber`
    Implemented `setProductProps`, which handles populating the properties `product_name`, and `product_price`, using `beforeInsert`, and `beforeUpdate` hooks

---
+++ src/modules/order-item/entities/order-item.subscriber.ts
@@ -0,0 +1,44 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { IProduct } from 'src/modules/product/interfaces/product.interface';
+import {
+  DataSource,
+  EntitySubscriberInterface,
+  EventSubscriber,
+  InsertEvent,
+  UpdateEvent,
+} from 'typeorm';
+import { OrderItem } from './order-item.entity';
+
+@Injectable()
+@EventSubscriber()
+export class OrderItemSubscriber implements EntitySubscriberInterface {
+  constructor(
+    private readonly datasource: DataSource,
+    @Inject(IProduct) private readonly productService: IProduct,
+  ) {
+    this.datasource.subscribers.push(this);
+  }
+
+  listenTo() {
+    return OrderItem;
+  }
+
+  async beforeInsert(event: InsertEvent<OrderItem>): Promise<void> {
+    await this.setProductProps(event);
+  }
+
+  async beforeUpdate(event: UpdateEvent<any>): Promise<void> {
+    await this.setProductProps(event);
+  }
+
+  async setProductProps(event: InsertEvent<OrderItem> | UpdateEvent<any>) {
+    if (event.entity && event.entity.product) {
+      const product = await this.productService.getProductOrFail(
+        event.entity.product.id,
+      );
+
+      event.entity.product_name = product.name;
+      event.entity.product_price = product.price;
+    }
+  }
+}
