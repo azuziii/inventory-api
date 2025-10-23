33b8e912cbdfc4bb5931b10267a46801a9edcdfd
Azuzi <azuzii@proton.me>
2025-10-23 15:31:15 +0100

    feat(order-item): Register `OrderItemSubscriber`

--- src/modules/order-item/order-item.module.ts
+++ src/modules/order-item/order-item.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductModule } from '../product/product.module';
 import { OrderItem } from './entities/order-item.entity';
+import { OrderItemSubscriber } from './entities/order-item.subscriber';
 import { OrderItemRepository } from './order-item.repository';
 import { OrderItemResolver } from './order-item.resolver';
 import { OrderItemService } from './order-item.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OrderItem]), ProductModule],
-  providers: [OrderItemResolver, OrderItemService, OrderItemRepository],
+  providers: [
+    OrderItemResolver,
+    OrderItemService,
+    OrderItemRepository,
+    OrderItemSubscriber,
+  ],
 })
 export class OrderItemModule {}
