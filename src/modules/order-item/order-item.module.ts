470e3ac87015310201188fe741467b1be60a6619
Azuzi <azuzii@proton.me>
2025-10-23 15:06:28 +0100

    feat(order-item): Import `ProductModule`

--- src/modules/order-item/order-item.module.ts
+++ src/modules/order-item/order-item.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from '../product/product.module';
 import { OrderItem } from './entities/order-item.entity';
 import { OrderItemRepository } from './order-item.repository';
 import { OrderItemResolver } from './order-item.resolver';
 import { OrderItemService } from './order-item.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderItem])],
+  imports: [TypeOrmModule.forFeature([OrderItem]), ProductModule],
   providers: [OrderItemResolver, OrderItemService, OrderItemRepository],
 })
 export class OrderItemModule {}
