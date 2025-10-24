9e7f84af8e5db78980a1d606ceded129e7170fa7
Azuzi <azuzii@proton.me>
2025-10-24 09:08:30 +0100

    feat(order-item-resolver): Add `getOrderItem` query

--- src/modules/order-item/order-item.resolver.ts
+++ src/modules/order-item/order-item.resolver.ts
@@ -1,4 +1,5 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { GetByIdArgs } from 'src/shared/args/get-by-id/get-by-id.args';
 import { ErrorResponseType } from 'src/shared/decorators/meta/error-response-type.decorator';
 import { OrderItem } from './entities/order-item.entity';
 import {
@@ -7,11 +8,21 @@ import {
 } from './inputs/order-item.input';
 import { OrderItemService } from './order-item.service';
 import { CreateOrderItemResponse } from './responses/create-order-item.response';
+import { OrderItemQueryResponse } from './responses/query-order-item.response';
 import { UpdateOrderItemResponse } from './responses/update-order-item.response';
 
 @Resolver(() => OrderItem)
 export class OrderItemResolver {
   constructor(private readonly orderItemService: OrderItemService) {}
+
+  @ErrorResponseType(OrderItemQueryResponse)
+  @Query(() => OrderItemQueryResponse, { name: 'orderItem' })
+  async getOrderItem(
+    @Args() { id }: GetByIdArgs,
+  ): Promise<OrderItemQueryResponse> {
+    const queryResult = await this.orderItemService.getOrderItemOrFail(id);
+    return new OrderItemQueryResponse(queryResult);
+  }
 
   @ErrorResponseType(CreateOrderItemResponse)
   @Mutation(() => CreateOrderItemResponse, { name: 'createOrderItem' })
