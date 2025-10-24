aa2f7dd793694435abecfb73cfd027a85346ec57
Azuzi <azuzii@proton.me>
2025-10-24 11:19:00 +0100

    feat(order-input): Add `items` property to `CreateOrderInput`, and pass custom message to IsUUID decorator

--- src/modules/order/inputs/order.input.ts
+++ src/modules/order/inputs/order.input.ts
@@ -1,5 +1,16 @@
 import { Field, InputType, Int, PartialType } from '@nestjs/graphql';
-import { IsDate, IsNotEmpty, IsNumber, IsUUID } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsUUID,
+  ValidateNested,
+} from 'class-validator';
+import { i18nValidationMessage } from 'nestjs-i18n';
+import { CreateOrderItemInput } from 'src/modules/order-item/inputs/order-item.input';
 import { WithUuidInputMixin } from 'src/shared/inputs/get-by-id/get-by-id.mixin';
 import { CreateOrderDto, UpdateOrderDto } from '../dto/order.dto';
 
@@ -17,8 +28,18 @@ export class CreateOrderInput implements CreateOrderDto {
 
   @Field()
   @IsNotEmpty()
-  @IsUUID()
+  @IsUUID('all', {
+    message: i18nValidationMessage('validation.invalid_uuid'),
+  })
   customer_id!: string;
+
+  @Field(() => [CreateOrderItemInput], { nullable: false })
+  @IsNotEmpty()
+  @IsArray()
+  @ArrayMinSize(1)
+  @Type(() => CreateOrderItemInput)
+  @ValidateNested({ each: true })
+  items!: CreateOrderItemInput[];
 }
 
 const UpdateOrderInputBase = WithUuidInputMixin(PartialType(CreateOrderInput));
