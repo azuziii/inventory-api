dd7c3d46d78188f29d40d29566703a2b1614b358
Azuzi <azuzii@proton.me>
2025-10-24 08:53:34 +0100

    feat(order-item-union): Define `UpdateOrderItemUnion`

---
+++ src/modules/order-item/unions/update-order-item.union.ts
@@ -0,0 +1,10 @@
+import { createUnionType } from '@nestjs/graphql';
+import { InvalidData } from 'src/shared/errors/invalid-data.error';
+import { OrderItem } from '../entities/order-item.entity';
+import { OrderItemNotFound } from '../errors/order-item.error';
+
+export type UpdateOrderItemUnion = typeof UpdateOrderItemUnion;
+export const UpdateOrderItemUnion = createUnionType({
+  name: 'UpdateOrderItemUnion',
+  types: () => [OrderItem, OrderItemNotFound, InvalidData],
+});
