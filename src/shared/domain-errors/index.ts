b649d8edb3d1c527a32fe3fe6d7eb29fc4455e87
Azuzi <azuzii@proton.me>
2025-10-24 11:21:34 +0100

    chore(domain-errros): Re-export `order`, and `order-item` errors

--- src/shared/domain-errors/index.ts
+++ src/shared/domain-errors/index.ts
@@ -1,2 +1,4 @@
 export * from 'src/modules/customer/errors/customer.error';
+export * from 'src/modules/order-item/errors/order-item.error';
+export * from 'src/modules/order/errors/order.error';
 export * from 'src/modules/product/errors/product.error';
