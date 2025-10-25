f35498931fc78fb3daa4de2fa360cf9e5ea0ff77
Azuzi <azuzii@proton.me>
2025-10-25 10:59:08 +0100

    feat(order-union): Add `ProductNotFound` to `CreateOrderUnion`

--- src/modules/order/unions/create-order.union.ts
+++ src/modules/order/unions/create-order.union.ts
@@ -1,5 +1,5 @@
 import { createUnionType } from '@nestjs/graphql';
-import { CustomerNotFound } from 'src/shared/domain-errors';
+import { CustomerNotFound, ProductNotFound } from 'src/shared/domain-errors';
 import { InvalidData } from 'src/shared/errors/invalid-data.error';
 import { Order } from '../entities/order.entity';
 import { OrderAlreadyExist } from '../errors/order.error';
@@ -7,5 +7,11 @@ import { OrderAlreadyExist } from '../errors/order.error';
 export type CreateOrderUnion = typeof CreateOrderUnion;
 export const CreateOrderUnion = createUnionType({
   name: 'CreateOrderUnion',
-  types: () => [Order, OrderAlreadyExist, InvalidData, CustomerNotFound],
+  types: () => [
+    Order,
+    OrderAlreadyExist,
+    InvalidData,
+    CustomerNotFound,
+    ProductNotFound,
+  ],
 });
