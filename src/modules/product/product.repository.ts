9c06000741d89c5f4ccf387717ca55e3c2ef9a96
Azuzi <azuzii@proton.me>
2025-10-31 09:58:57 +0100

    refactor(product-repository): Remove manual setting of `customer_id` property, and removed manually assigning `id` to `customer` property

--- src/modules/product/product.repository.ts
+++ src/modules/product/product.repository.ts
@@ -1,7 +1,6 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { BaseRepositoty } from 'src/shared/base/repository';
 import { EntityManager } from 'typeorm';
-import { Customer } from '../customer/entities/customer.entity';
 import { CreateProductDto, UpdateProductDto } from './dto/product.dto';
 import { Product } from './entities/product.entity';
 import { ProductAlreadyExist } from './errors/product.error';
@@ -13,17 +12,12 @@ export class ProductRepository extends BaseRepositoty<Product> {
   }
 
   async insertProduct(
-    { customer_id, ...product }: CreateProductDto,
+    product: CreateProductDto,
     entityManager?: EntityManager,
   ): Promise<Product> {
     const manager = this.getManager(entityManager);
 
-    const newProduct = manager.create(Product, {
-      ...product,
-      customer: {
-        id: customer_id,
-      },
-    });
+    const newProduct = manager.create(Product, product);
 
     try {
       const insertResult = await manager.insert(Product, newProduct);
@@ -38,19 +32,13 @@ export class ProductRepository extends BaseRepositoty<Product> {
   }
 
   async updateProduct(
-    { customer_id, id, ...product }: UpdateProductDto,
+    { id, ...product }: UpdateProductDto,
     entityManager?: EntityManager,
   ): Promise<Product> {
     try {
       const manager = this.getManager(entityManager);
 
       const productPayload: Partial<Product> = product;
-
-      if (customer_id) {
-        productPayload.customer = {
-          id: customer_id,
-        } as Customer;
-      }
 
       await manager.update(Product, id, productPayload);
       return manager.findOne(Product, {
