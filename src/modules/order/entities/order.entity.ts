8942cdd23b6fbc4352663a4a32048f220211aed9
Azuzi <azuzii@proton.me>
2025-11-12 17:48:14 +0100

    refactor(order-entity): Remove graphql decorators from `Order`

--- src/modules/order/entities/order.entity.ts
+++ src/modules/order/entities/order.entity.ts
@@ -1,7 +1,8 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field } from '@nestjs/graphql';
 import { Customer } from 'src/modules/customer/entities/customer.entity';
 import { CustomerOutput } from 'src/modules/customer/outputs/customer.output';
 import { OrderItem } from 'src/modules/order-item/entities/order-item.entity';
+import { OrderItemOutput } from 'src/modules/order-item/outputs/order-item.output';
 import { BaseUUIDEntity } from 'src/shared/base/base.entity';
 import {
   BeforeInsert,
@@ -16,7 +17,6 @@ import {
 
 @Entity('order')
 @Index('UQ_ORDER_NUMBER_YEAR', ['order_year', 'order_number'], { unique: true })
-@ObjectType()
 export class Order extends BaseUUIDEntity {
   @Field({ nullable: false })
   @Column({ type: 'text' })
@@ -41,7 +41,7 @@ export class Order extends BaseUUIDEntity {
   @Column({ type: 'uuid' })
   customer_id!: string;
 
-  @Field(() => [OrderItem!], { nullable: false })
+  @Field(() => [OrderItemOutput!], { nullable: false })
   @OneToMany(() => OrderItem, (item) => item.order, { lazy: true })
   items!: OrderItem[];
 
