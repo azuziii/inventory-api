3061cb85bc1255bdac9810f52e32ef2e1bcd45dd
Azuzi <azuzii@proton.me>
2025-11-12 17:13:57 +0100

    refactor(order-item-entity): Remove graphql decorators from `OrderItem`

--- src/modules/order-item/entities/order-item.entity.ts
+++ src/modules/order-item/entities/order-item.entity.ts
@@ -1,37 +1,29 @@
-import { Field, Float, Int, ObjectType } from '@nestjs/graphql';
 import { Order } from 'src/modules/order/entities/order.entity';
 import { Product } from 'src/modules/product/entities/product.entity';
-import { ProductOutput } from 'src/modules/product/outputs/product.output';
 import { BaseUUIDEntity } from 'src/shared/base/base.entity';
 import { InvalidDataException } from 'src/shared/errors/invalid-data.error';
 import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 @Entity('order_item')
-@ObjectType()
 export class OrderItem extends BaseUUIDEntity {
-  @Field()
   @Column({ type: 'text', nullable: false })
   product_name!: string;
 
-  @Field(() => Float)
   @Column({ type: 'float', nullable: false })
   product_price!: number;
 
-  @Field(() => Int, { defaultValue: 0 })
   @Column({ type: 'int', nullable: false, default: 0 })
   quantity!: number;
 
-  @Field(() => Int, { defaultValue: 0 })
   @Column({ type: 'int', nullable: false, default: 0 })
   total_shipped!: number;
 
-  @Field(() => ProductOutput)
   @ManyToOne(() => Product, { lazy: true })
   @JoinColumn({
     name: 'product_id',
     foreignKeyConstraintName: 'FK_PRODUCT_ORDER_ITEM',
   })
-  product!: Promise<Order>;
+  product!: Promise<Product>;
 
   @Column({ type: 'uuid' })
   product_id!: string;
