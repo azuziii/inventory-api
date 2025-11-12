fa572826144af3e443975fea16082a78ddf9ee3d
Azuzi <azuzii@proton.me>
2025-11-12 16:39:01 +0100

    refactor(product-entity): Remove graphql decorators from `Product`

--- src/modules/product/entities/product.entity.ts
+++ src/modules/product/entities/product.entity.ts
@@ -1,23 +1,17 @@
-import { Field, Float, ObjectType } from '@nestjs/graphql';
 import { Customer } from 'src/modules/customer/entities/customer.entity';
-import { CustomerOutput } from 'src/modules/customer/outputs/customer.output';
 import { BaseUUIDEntity } from 'src/shared/base/base.entity';
 import { ColumnNumericTransformer } from 'src/shared/db-transformers/numeric.transformer';
 import { Column, Entity, JoinColumn, ManyToOne, Unique } from 'typeorm';
 
-@ObjectType()
 @Entity('product')
 @Unique('UQ_product_customer_name', ['customer', 'name'])
 export class Product extends BaseUUIDEntity {
-  @Field({ nullable: false })
   @Column({ type: 'text' })
   name!: string;
 
-  @Field({ nullable: false })
   @Column({ type: 'text' })
   code!: string;
 
-  @Field(() => Float, { nullable: false, defaultValue: 0 })
   @Column({
     type: 'float',
     default: 0,
@@ -25,11 +19,9 @@ export class Product extends BaseUUIDEntity {
   })
   price!: number;
 
-  @Field({ nullable: false, defaultValue: false })
   @Column({ type: 'boolean', default: false })
   isSample!: boolean;
 
-  @Field(() => CustomerOutput, { nullable: false })
   @ManyToOne(() => Customer, { lazy: true })
   @JoinColumn({
     name: 'customer_id',
