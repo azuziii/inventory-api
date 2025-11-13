18b32d001d64ff9364071aba140338fa2262cfa1
Azuzi <azuzii@proton.me>
2025-11-13 09:51:41 +0100

    feat(order-resolver): Define resolver return type, and added `customer` and `items` resolvers

--- src/modules/order/order.resolver.ts
+++ src/modules/order/order.resolver.ts
@@ -1,9 +1,18 @@
 import { UsePipes } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { GetByIdArgs } from 'src/shared/args/get-by-id/get-by-id.args';
 import { AutoMap } from 'src/shared/decorators/meta/auto-map.decorator';
 import { ResponseType } from 'src/shared/decorators/meta/error-response-type.decorator';
 import { mapToOutput } from 'src/utils/map-to-output.util';
+import { CustomerOutput } from '../customer/outputs/customer.output';
+import { OrderItemOutput } from '../order-item/outputs/order-item.output';
 import { OrderArguments } from './args/order.args';
 import { Order } from './entities/order.entity';
 import { CreateOrderInput, UpdateOrderInput } from './inputs/order.input';
@@ -16,7 +25,7 @@ import { OrdersQueryResponse } from './responses/list-orders.response';
 import { OrderQueryResponse } from './responses/query-order.response';
 import { UpdateOrderResponse } from './responses/update-order.response';
 
-@Resolver()
+@Resolver(() => OrderOutput)
 export class OrderResolver {
   constructor(private readonly orderService: OrderService) {}
 
@@ -61,5 +70,17 @@ export class OrderResolver {
     input: UpdateOrderInput,
   ): Promise<Order> {
     return this.orderService.updateOrder(input);
+  }
+
+  @ResolveField(() => CustomerOutput)
+  async customer(@Parent() order: Order): Promise<CustomerOutput> {
+    const customer = await order.customer;
+    return mapToOutput(CustomerOutput, customer);
+  }
+
+  @ResolveField(() => [OrderItemOutput])
+  async items(@Parent() order: Order): Promise<OrderItemOutput[]> {
+    const items = await order.items;
+    return mapToOutput(OrderItemOutput, items);
   }
 }
