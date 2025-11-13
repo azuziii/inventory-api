3a8648b4c5a85f55bd151cbf926a35c626bad07e
Azuzi <azuzii@proton.me>
2025-11-13 09:35:52 +0100

    feat(output): Add `Expose` decorator to properties

--- src/modules/order-item/outputs/order-item.output.ts
+++ src/modules/order-item/outputs/order-item.output.ts
@@ -1,20 +1,31 @@
-import { Field, Float, Int, ObjectType } from '@nestjs/graphql';
+import { Field, Float, ID, Int, ObjectType } from '@nestjs/graphql';
+import { Expose, Type } from 'class-transformer';
 import { ProductOutput } from 'src/modules/product/outputs/product.output';
 
-@ObjectType('orderItem')
+@ObjectType('OrderItem')
 export class OrderItemOutput {
+  @Field(() => ID)
+  @Expose()
+  id!: string;
+
   @Field()
+  @Expose()
   product_name!: string;
 
   @Field(() => Float)
+  @Expose()
   product_price!: number;
 
   @Field(() => Int, { defaultValue: 0 })
+  @Expose()
   quantity!: number;
 
   @Field(() => Int, { defaultValue: 0 })
+  @Expose()
   total_shipped!: number;
 
   @Field(() => ProductOutput)
+  @Type(() => ProductOutput)
+  @Expose()
   product!: ProductOutput;
 }
--- src/modules/order/outputs/order.output.ts
+++ src/modules/order/outputs/order.output.ts
@@ -1,21 +1,33 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Expose, Type } from 'class-transformer';
 import { CustomerOutput } from 'src/modules/customer/outputs/customer.output';
 import { OrderItemOutput } from 'src/modules/order-item/outputs/order-item.output';
 
 @ObjectType('Order')
 export class OrderOutput {
+  @Field(() => ID)
+  @Expose()
+  id!: string;
+
   @Field({ nullable: false })
+  @Expose()
   order_number!: string;
 
   @Field(() => Date, { nullable: false })
+  @Expose()
   order_date!: Date;
 
   @Field({ nullable: false })
+  @Expose()
   order_year!: string;
 
   @Field(() => CustomerOutput, { nullable: false })
+  @Type(() => CustomerOutput)
+  @Expose()
   customer!: CustomerOutput;
 
   @Field(() => [OrderItemOutput!], { nullable: false })
+  @Type(() => OrderItemOutput)
+  @Expose()
   items!: OrderItemOutput[];
 }
