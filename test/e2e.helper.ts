63774ec8152a6137ebabab6b569f7dd273ff9b95
Azuzi <azuzii@proton.me>
2025-11-29 17:25:53 +0100

    feat(e2e-helpers): Create helper functions.
    - `clearDB` handles clearing all tables
    
    - `initializeTestEnvironment` handles setting up the testing enviroment
    
    - `cleanupTestEnvironment` handles clearing up the testing enviroment

---
+++ test/e2e.helper.ts
@@ -0,0 +1,60 @@
+import { INestApplication } from '@nestjs/common';
+import { TestingModule } from '@nestjs/testing';
+import { I18nValidationPipe } from 'nestjs-i18n';
+import { DataSource } from 'typeorm';
+import { createE2ETestingModule } from './e2e-testing-module';
+
+export async function clearDB(app: INestApplication) {
+  const dataSource = app.get(DataSource);
+  if (!dataSource) {
+    throw new Error(
+      'Failed to retrieve TypeORM DataSource from NestJS application.',
+    );
+  }
+  const entities = app.get(DataSource).entityMetadatas;
+  for (const entity of entities) {
+    const repository = app.get(DataSource).getRepository(entity.name);
+    await repository.query(
+      `TRUNCATE "${entity.tableName}" RESTART IDENTITY CASCADE;`,
+    );
+  }
+}
+
+export async function initializeTestEnvironment(): Promise<
+  [INestApplication, TestingModule]
+> {
+  try {
+    const module = await createE2ETestingModule();
+    const app = module.createNestApplication();
+
+    app.useGlobalPipes(
+      new I18nValidationPipe({
+        transform: true,
+        whitelist: true,
+      }),
+    );
+
+    await app.init();
+
+    return [app, module];
+  } catch (error) {
+    console.error('Failed to initialize test enviroment:', error);
+    process.exit(1);
+  }
+}
+
+export async function cleanupTestEnvironment(
+  module: TestingModule,
+  app: INestApplication,
+) {
+  try {
+    if (app) {
+      await clearDB(app);
+      await app.close();
+    }
+    if (module) await module.close();
+  } catch (error) {
+    console.error('Failed to cleanup testi enviroment:', error);
+    process.exit(1);
+  }
+}
