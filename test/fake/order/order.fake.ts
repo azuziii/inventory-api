22c2944e0f31d7bd934a43959f43da6e2f742f0b
Azuzi <azuzii@proton.me>
2025-11-29 17:32:35 +0100

    feat(order-e2e): Create fake data helper functions

---
+++ test/fake/order/order.fake.ts
@@ -0,0 +1,38 @@
+import { faker } from '@faker-js/faker';
+import { CreateOrderInput } from 'src/modules/order/inputs/order.input';
+import { createRandomOrderItemInput } from '../order-item/order-item.fake';
+
+export function createRandomOrderInput(): CreateOrderInput {
+  return {
+    customer_id: faker.string.uuid(),
+    order_date: faker.date.anytime(),
+    order_number: faker.number
+      .int({
+        min: 1,
+        max: 100000000,
+      })
+      .toString(),
+    items: [],
+  };
+}
+
+export function createRandomOrderInputWithItems(
+  itemsLength: number = 1,
+): CreateOrderInput {
+  const items = Array.from({ length: itemsLength }, () =>
+    createRandomOrderItemInput(),
+  );
+
+  return {
+    customer_id: faker.string.uuid(),
+    order_date: faker.date.anytime(),
+    order_number: faker.string.numeric({
+      allowLeadingZeros: true,
+      length: {
+        min: 1,
+        max: 5,
+      },
+    }),
+    items: [createRandomOrderItemInput()],
+  };
+}
