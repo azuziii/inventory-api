ab3448fed3ab05d3f61f2904bf82e20abb9de566
Azuzi <azuzii@proton.me>
2025-12-04 14:07:48 +0100

    feat(shipment-input): Create `CreateShipmentInput`

---
+++ src/modules/shipment/inputs/create-shipment.input.ts
@@ -0,0 +1,30 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsDate, IsEnum, IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { i18nValidationMessage } from 'nestjs-i18n';
+import { CreateShipmentDto } from '../dto/create-shipment.dto';
+import { ShipmentType } from '../enums/shipment-type.enum';
+
+@InputType()
+export class CreateShipmentInput implements CreateShipmentDto {
+  @Field(() => Date)
+  @IsNotEmpty()
+  @IsDate()
+  delivery_date!: Date;
+
+  @Field()
+  @IsNotEmpty()
+  @IsUUID('all', {
+    message: i18nValidationMessage('validation.invalid_uuid'),
+  })
+  customer_id!: string;
+
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  shipment_number!: string;
+
+  @Field(() => ShipmentType, { defaultValue: ShipmentType.Outbound })
+  @IsNotEmpty()
+  @IsEnum(ShipmentType)
+  shipment_type!: ShipmentType;
+}
