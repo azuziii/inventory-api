49a9624647def73399e3703c95b789facbe2c0b8
Azuzi <azuzii@proton.me>
2025-12-05 11:53:03 +0100

    feat(shipment-errors): Define `ShipmentNotFound` error

--- src/modules/shipment/errors/shipment.error.ts
+++ src/modules/shipment/errors/shipment.error.ts
@@ -1,5 +1,7 @@
 import { AlreadyExist } from 'src/shared/errors/already-exist.error';
+import { NotFound } from 'src/shared/errors/not-found.error';
 
 const ENTITY_NAME = 'Shipment';
 
 export const ShipmentAlreadyExist = AlreadyExist(ENTITY_NAME);
+export const ShipmentNotFound = NotFound(ENTITY_NAME);
