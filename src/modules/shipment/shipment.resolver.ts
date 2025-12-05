af249b57888237543d8ead5449067e9166ed5168
Azuzi <azuzii@proton.me>
2025-12-05 16:38:51 +0100

    feat(shipment-resolver): Add `updateShipment` resolver

--- src/modules/shipment/shipment.resolver.ts
+++ src/modules/shipment/shipment.resolver.ts
@@ -4,8 +4,10 @@ import { AutoMap } from 'src/shared/decorators/meta/auto-map.decorator';
 import { ResponseType } from 'src/shared/decorators/meta/error-response-type.decorator';
 import { Shipment } from './entities/shipment.entity';
 import { CreateShipmentInput } from './inputs/create-shipment.input';
+import { UpdateShipmentInput } from './inputs/update-shipment.input';
 import { ShipmentOutput } from './outputs/shipment.output';
 import { CreateShipmentResponse } from './responses/create-shipment.response';
+import { UpdateShipmentResponse } from './responses/update-shipment.response';
 import { ShipmentService } from './shipment.service';
 
 @Injectable()
@@ -19,5 +21,15 @@ export class ShipmentResolver {
     @Args('input') input: CreateShipmentInput,
   ): Promise<Shipment> {
     return this.shipmentService.createShipment(input);
+  }
+
+  @ResponseType(UpdateShipmentResponse)
+  @AutoMap(ShipmentOutput)
+  @Mutation(() => UpdateShipmentResponse, { name: 'updateShipment' })
+  async updateShipment(
+    @Args('input', { type: () => UpdateShipmentInput, nullable: false })
+    input: UpdateShipmentInput,
+  ): Promise<Shipment> {
+    return this.shipmentService.updateShipment(input);
   }
 }
