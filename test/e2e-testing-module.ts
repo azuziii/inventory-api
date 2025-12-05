c05c0310955b78a9489624b0b4ca2c64da859cf2
Azuzi <azuzii@proton.me>
2025-12-05 09:10:02 +0100

    refactor(e2e): Rename `E2ETestConfig` to `E2ETypeormConfig`

--- test/e2e-testing-module.ts
+++ test/e2e-testing-module.ts
@@ -1,7 +1,7 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
 import { TypeormConfig } from 'src/config/typeorm-config.service';
-import { E2ETestConfig } from './config/e2e-typeorm-config.service';
+import { E2ETypeormConfig } from './config/e2e-typeorm-config.service';
 
 export const createE2ETestingModule = async () => {
   const moduleBuilder = Test.createTestingModule(
@@ -12,7 +12,7 @@ export const createE2ETestingModule = async () => {
       moduleIdGeneratorAlgorithm: 'deep-hash',
     },
   );
-  moduleBuilder.overrideProvider(TypeormConfig).useClass(E2ETestConfig);
+  moduleBuilder.overrideProvider(TypeormConfig).useClass(E2ETypeormConfig);
 
   return moduleBuilder.compile();
 };
