2e73ba24c6002831dd1381e629adb41a171ae982
Azuzi <azuzii@proton.me>
2025-12-06 09:39:47 +0100

    feat(shipment-resolver): Add `deleteShipment` mutation

--- src/modules/shipment/shipment.resolver.ts
+++ src/modules/shipment/shipment.resolver.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { Args, Mutation } from '@nestjs/graphql';
+import { GetByIdArgs } from 'src/shared/args/get-by-id/get-by-id.args';
 import { AutoMap } from 'src/shared/decorators/meta/auto-map.decorator';
 import { ResponseType } from 'src/shared/decorators/meta/error-response-type.decorator';
+import { DeleteResponse } from 'src/shared/responses/delete.response';
 import { Shipment } from './entities/shipment.entity';
 import { CreateShipmentInput } from './inputs/create-shipment.input';
 import { UpdateShipmentInput } from './inputs/update-shipment.input';
 import { ShipmentOutput } from './outputs/shipment.output';
 import { CreateShipmentResponse } from './responses/create-shipment.response';
+import { DeleteShipmentResponse } from './responses/delete-shipment.response';
 import { UpdateShipmentResponse } from './responses/update-shipment.response';
 import { ShipmentService } from './shipment.service';
 
@@ -31,5 +34,11 @@ export class ShipmentResolver {
     input: UpdateShipmentInput,
   ): Promise<Shipment> {
     return this.shipmentService.updateShipment(input);
+  }
+
+  @ResponseType(DeleteShipmentResponse)
+  @Mutation(() => DeleteShipmentResponse, { name: 'deleteShipment' })
+  async deleteShipment(@Args() { id }: GetByIdArgs): Promise<DeleteResponse> {
+    return this.shipmentService.deleteShipment(id);
   }
 }
