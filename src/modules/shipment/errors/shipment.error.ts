1cff551ba10b5f17fc3aea515eaa2ebae09ed079
Azuzi <azuzii@proton.me>
2025-12-06 09:37:30 +0100

    feat(shipment-errors): Define `ShipmentInUse` error

--- src/modules/shipment/errors/shipment.error.ts
+++ src/modules/shipment/errors/shipment.error.ts
@@ -1,7 +1,9 @@
 import { AlreadyExist } from 'src/shared/errors/already-exist.error';
+import { InUse } from 'src/shared/errors/in-use.error';
 import { NotFound } from 'src/shared/errors/not-found.error';
 
 const ENTITY_NAME = 'Shipment';
 
 export const ShipmentAlreadyExist = AlreadyExist(ENTITY_NAME);
 export const ShipmentNotFound = NotFound(ENTITY_NAME);
+export const ShipmentInUse = InUse(ENTITY_NAME);
